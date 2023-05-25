8de2c68651ea7b2f3eebe4e41af8bdc713013a2c
DavideAtzo <atzo9477@gmail.com>
2023-05-25 15:48:00 +0200

    milestone 4

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,36 @@
 <script>
+import axios from 'axios';
 import AppHeader from './components/AppHeader.vue';
 import AppMain from './components/AppMain.vue';
 export default {
-  name:'App',
+  name: 'App',
+  data() {
+    return {
+      apiBaseUrl: 'http://127.0.0.1:8000/api',
+      apiUrls: {
+        projects: '/projects'
+      },
+      projects: []
+    }
+  },
   components: {
     AppHeader,
     AppMain
+  },
+  methods: {
+    getProjects() {
+      axios.get(this.apiBaseUrl + this.apiUrls.projects)
+      .then((response) => {
+        this.projects = response.data.results;
+        console.log(response);
+      })
+      .catch((error) => {
+        console.log(error);
+      })
+    }
+  },
+  created() {
+    this.getProjects();
   }
 }
 </script>
