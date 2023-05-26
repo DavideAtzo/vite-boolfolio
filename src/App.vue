7d0a1516788fdf920ff3e4ea4e486a1f5f9d1873
DavideAtzo <atzo9477@gmail.com>
2023-05-26 17:32:45 +0200

    fix

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,15 @@
 <script>
 import AppHeader from './components/AppHeader.vue';
-import AppMain from './components/AppMain.vue';
 export default {
   name: 'App',
   components: {
     AppHeader,
-    AppMain
   },
 }
 </script>
 
 <template>
   <AppHeader />
-  <AppMain />
   <router-view />
 </template>
 
