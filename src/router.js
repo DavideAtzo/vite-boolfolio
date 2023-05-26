8bc12f051a01b31c7a6f9271cafb187c37155eba
DavideAtzo <atzo9477@gmail.com>
2023-05-26 15:19:20 +0200

    creato file js per le rotte

---
+++ src/router.js
@@ -0,0 +1,12 @@
+import { createWebHistory, createRouter } from 'vue-router';
+
+import AppHome from './pages/AppHome.vue';
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes: [
+        //
+        ]
+});
+
+export { router };
