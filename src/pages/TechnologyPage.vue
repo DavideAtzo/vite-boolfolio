a2fe6ea0fb928693472f634324b273b6c312a607
DavideAtzo <atzo9477@gmail.com>
2023-05-30 00:41:39 +0200

    visualizzo technologies in base alla select

--- src/pages/TechnologyPage.vue
+++ src/pages/TechnologyPage.vue
@@ -1,13 +1,49 @@
 <script>
-    export default {
-        
+import axios from 'axios';
+import ProjectCard from '../components/ProjectCard.vue';
+export default {
+    name: 'TechnologyPage',
+    components: {
+        ProjectCard
+    },
+    data() {
+        return {
+            apiBaseUrl: 'http://127.0.0.1:8000/api',
+            technology: null,
+        }
+    },
+    methods: {
+        getTechnology() {
+            axios.get(this.apiBaseUrl + `/technologies/${this.$route.params.slug}`)
+                .then((response) => {
+                    this.technology = response.data.results
+                    console.log(response)
+                })
+        }
+    },
+    created() {
+        this.getTechnology();
+        this.$watch(
+            () => this.$route.params,
+            (toParams, previousParams) => {
+                this.getTechnology();
+            }
+        )
     }
+}
 </script>
 
 <template>
-    <div>
-        sono tech page
-    </div>
+    <section v-if="technology">
+        <div class="container">
+            <h1 class="mt-5 mb-3">Technology Page</h1>
+            <div class="row my-4 gy-4">
+                <div class="card col-md-3" v-for="project in technology.projects">
+                    <ProjectCard :project="project"></ProjectCard>
+                </div>
+            </div>
+        </div>
+    </section>
 </template>
 
 <style lang="scss" scoped>
